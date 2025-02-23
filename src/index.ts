9c4345aee8783e01b4dd5b7d45b5b585e39b592c
Eisenheim <cto@rootsedulive.com>
2025-02-23 18:33:11 +0600

    Add static file route

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { serve } from '@hono/node-server'
+import { serveStatic } from '@hono/node-server/serve-static'
 import { Hono } from 'hono'
 
 const app = new Hono()
@@ -6,6 +7,8 @@ const app = new Hono()
 app.get('/', (c) => {
   return c.text('Server is running...')
 })
+
+app.use('*', serveStatic({ root: './static' }))
 
 serve({
   fetch: app.fetch,
